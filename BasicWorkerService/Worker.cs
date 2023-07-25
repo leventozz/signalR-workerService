8f3e61426ad84d5b146b8309f3ef3c7443810d9b
lozturk <leventufukozturk@gmail.com>
2023-07-25 21:44:13 +0300

    configured worker service

--- BasicWorkerService/Worker.cs
+++ BasicWorkerService/Worker.cs
@@ -1,12 +1,18 @@
+using Microsoft.AspNetCore.SignalR;
+using SignalR;
+
 namespace BasicWorkerService
 {
 	public class Worker : BackgroundService
 	{
 		private readonly ILogger<Worker> _logger;
+		private readonly IHubContext<StockHub, IStockService> _stockHub;
+		private const string stockName = "Basic Stock Name";
 
-		public Worker(ILogger<Worker> logger)
+		public Worker(ILogger<Worker> logger, IHubContext<StockHub, IStockService> stockHub)
 		{
 			_logger = logger;
+			_stockHub = stockHub;
 		}
 
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -14,6 +20,9 @@ namespace BasicWorkerService
 			while (!stoppingToken.IsCancellationRequested)
 			{
 				_logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+				Random random = new Random();
+				decimal stockPrice = random.Next(1, 100);
+				await _stockHub.Clients.All.SendStockPrice(stockName, stockPrice);
 				await Task.Delay(1000, stoppingToken);
 			}
 		}
